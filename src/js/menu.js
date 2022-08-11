5acb12c87dd516cf06bcc8b57ba78f0c2882710c
Alexei Smahrovych <smagrovich58@gmail.com>
2022-08-11 16:29:04 +0300

    header__menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,30 +1,25 @@
-(() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-    const burgMenuBtn = document.querySelector('.js-open-menu');
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-      burgMenuBtn.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 1200px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
+
+
+  (()=> {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const menuBtnClose = document.querySelector("[data-close-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const headerBtn = document.querySelector("[data-header-btn]");
+  menuBtnRef.addEventListener("click", () => {
+    const expanded=
+     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.classList.add("is-open");
+    menuBtnRef.setAttribute("artia-expanded", !expanded);
+    mobileMenuRef.classList.add("is-open");
+    headerBtn.classList.add("is-open");
+  });
+  menuBtnClose.addEventListener("click", () => {
+    const expanded=
+     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    menuBtnRef.classList.remove("is-open");
+    menuBtnRef.setAttribute("artia-expanded", !expanded);
+    mobileMenuRef.classList.remove("is-open");
+    headerBtn.classList.remove("is-open");
+  });
+
+ })();                                  
