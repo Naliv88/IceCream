a0ff3d6f65a47687cf504fec753a2fbc62002152
Alexei Smahrovych <smagrovich58@gmail.com>
2022-08-12 14:14:22 +0300

    bugfix-to top

--- src/js/menu.js
+++ src/js/menu.js
@@ -44,28 +44,10 @@
 	});
 });
 
- $(document).ready(function(){
-	$("#menu").on("click","button", function (event) {
-    
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
-});
-
 $(function() {
 
 let nav = $("#nav");
 let navToggle = $("#navToggle");
-
 
    /* Smooth scroll */
 
@@ -82,10 +64,7 @@ scrollTop: elementOffset - 70
 });
 });
 
-
   /* Nav Toggle */
-
-
 
 $("#navToggle").on("click", function(event) {
 event.preventDefault();
@@ -94,6 +73,35 @@ nav.toggleClass("show");
 
 });
 
+});
 
+$(function() {
+
+let nav = $("#hero");
+let navToggle = $("#navToggle");
+
+   /* Smooth scroll */
+
+$("[data-scroll]").on("click", function(event) {
+event.preventDefault();
+
+let elementId = $(this).data('scroll');
+let elementOffset = $(elementId).offset().top;
+
+nav.removeClass("show");
+
+$("html, body").animate({
+scrollTop: elementOffset - 70
+});
+});
+
+  /* Nav Toggle */
+
+$("#navToggle").on("click", function(event) {
+event.preventDefault();
+
+nav.toggleClass("show");
+
+});
 
 });
--- src/js/to-top.js
+++ src/js/to-top.js
@@ -1,18 +1,26 @@
 // When the user scrolls down 20px from the top of the document, show the button
-// $(function() {
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        document.getElementById("top-btn").style.display = "block";
+        document.getElementById("top-btn").style.display = "flex";
     } else {
         document.getElementById("top-btn").style.display = "none";
     }
 }
+    
+$(function() {
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-// });
+    $("[data-scroll]").on("click", function(event) {
+    event.preventDefault();
+    
+    let elementId = $(this).data('scroll');
+    let elementOffset = $(elementId).offset().top;
+    
+    $("html, body").animate({
+    scrollTop: elementOffset - 70
+    });
+    });
+
+});
