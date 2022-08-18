0e5e3572be2118098186ece41820685c31fab55e
Alexei Smahrovych <smagrovich58@gmail.com>
2022-08-18 12:24:05 +0300

    modal-window_bugfix

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,8 +1,42 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openModalBtn: document.querySelector("[data-location-open]"),
+      closeModalBtn: document.querySelector("[data-location-close]"),
+      modal: document.querySelector("[data-location]"),
+      body: document.querySelector("[data-body]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
+    }
+  })();
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-franchise-open]"),
+      closeModalBtn: document.querySelector("[data-franchise-close]"),
+      modal: document.querySelector("[data-franchise]"),
+      body: document.querySelector("[data-body]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
+    }
+  })();
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-buy-open]"),
+      closeModalBtn: document.querySelector("[data-buy-close]"),
+      modal: document.querySelector("[data-buy]"),
       body: document.querySelector("[data-body]"),
     };
   
