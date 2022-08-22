036eab6959486fc1230ec85f51d80163774852c6
Alexei Smahrovych <smagrovich58@gmail.com>
2022-08-22 23:31:31 +0300

    bug-fix-header-about

--- src/js/about.js
+++ src/js/about.js
@@ -1,25 +1,9 @@
-// (() => {
-//     const refs = {
-//       readMoreBtn: document.querySelector("[data-read-more]"),
-//       readCloseBtn: document.querySelector("[data-read-close]"),
-//       history: document.querySelector("[data-history]"),
-//     };
-  
-//     refs.readMoreBtn.addEventListener("click", toggleModal);
-//     refs.readCloseBtn.addEventListener("click", toggleModal);
 
-  
-//     function toggleModal() {
-//       refs.history.classList.toggle("is-more");
-//     }
-//   })();
-// (() => {
-    const history = document.querySelector("[data-history]");
-    const readMoreBtn = document.querySelector("[data-read-more]");
-    const readCloseBtn = document.querySelector("[data-read-close]");
-    
+    const history = document.querySelectorAll("[data-history]");
     history.forEach((more) => {
-
+        const readMoreBtn = document.querySelector("[data-read-more]");
+        const readCloseBtn = document.querySelector("[data-read-close]");
+        
         readMoreBtn.addEventListener("click", toggleModal);
         readCloseBtn.addEventListener("click", toggleModal);
 
@@ -27,7 +11,7 @@
           more.classList.toggle("is-more");
         }
 });  
-// })();
+
 
 
 
